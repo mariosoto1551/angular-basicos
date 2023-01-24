307f8186bccfbe9c66c6a41245af912944f472a5
mariosoto1551 <mariosoto1551@gmail.com>
2023-01-24 11:21:57 -0400

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  title: string = 'bases';
+  numero: number = 0;
+
+  base: number = 5;
+
+  acumular( valor: number ){
+    this.numero += valor;
+  }
+  
+
 }
+
